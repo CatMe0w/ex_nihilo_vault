7b40d1f37e9f5957cc4c27cfb1b849f304350ba3
CatMe0w <CatMe0w@live.cn>
2022-03-06 21:53:13 +0800

    Remove referer to stop bilibili's tracking

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #[macro_use]
 extern crate rocket;
-use rocket::response::Redirect;
+use rocket::response::content::Html;
 use rocket::serde::{json::Json, Deserialize, Serialize};
 
 #[get("/")]
-fn rickroll() -> Redirect {
-    Redirect::to("https://www.bilibili.com/video/BV1va411w7aM")
+fn rickroll() -> Html<&'static str> {
+    Html("<!doctype html><meta name='referrer' content='no-referrer'><meta http-equiv='refresh' content='0; URL=https://www.bilibili.com/video/av202867917'>")
 }
 
 #[launch]
